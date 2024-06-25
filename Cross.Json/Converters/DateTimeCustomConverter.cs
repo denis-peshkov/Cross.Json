54847e70d1d7b3df7c6e42f80d1fdfb46232948a
Andrii Baranov <36504441+AndriiBaranov@users.noreply.github.com>
2024-06-25 16:29:23 +0300

    Add new DateTimeUtcConverter.cs (#2)

---
+++ Cross.Json/Converters/DateTimeCustomConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace Cross.Json.Converters;
+
+public class DateTimeUtcConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (DateTime.TryParse(reader.GetString(), out var parsedDateTime))
+        {
+            return parsedDateTime;
+        }
+
+        throw new ArgumentException($"{nameof(DateTimeUtcConverter)}.Read() => Cannot format {reader.GetString()} to DateTime");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("yyyy-MM-ddThh:mm:ss.fffZ", CultureInfo.InvariantCulture));
+    }
+}
